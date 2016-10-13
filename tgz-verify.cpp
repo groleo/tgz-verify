47cba828da8996a4cad9d7ef564f697295a9f078
Adrian Negreanu <groleo@gmail.com>
2016-10-13 20:02:55 +0300

    fix leaks and errors reported by valgrind

--- tgz-verify.cpp
+++ tgz-verify.cpp
@@ -9,37 +9,65 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+
 #include <axTLS/crypto.h>
 #include <axTLS/crypto_misc.h>
+
 
 class Goopa
 {
 private:
-    int _fd;
     off_t _sig_off;
     off_t _sizeof_sig;
     off_t _sizeof_dgst;
     off_t _sizeof_pkcs_mark;
+    off_t _sizeof_dgst_mark;
     const char* _tgz_name;
     const char* _tgz_basename;
     const char* _key_name;
 private:
-    uint8_t* _sig=nullptr;
-    uint8_t* _dgstD=nullptr;
-    uint8_t* _dgstC=nullptr;
+    int _fd=-1;
+    uint8_t* _sig=0;
+    uint8_t* _dgstD=0;
+    uint8_t* _dgstC=0;
     const off_t _sizeof_pkcs_modulo = 256;
 
 public:
-    Goopa(const char* t, const char* k)
-        : _tgz_name(t)
-        , _tgz_basename(basename(t))
-        , _key_name(k)
+    Goopa()
     {
-        Clean();
+        clean();
     }
-    bool Init()
+    ~Goopa()
     {
-        assert(_tgz_name!=nullptr);
+        clean();
+    }
+    bool Verify(const char* t, const char* k)
+    {
+        if ( ! init(t,k)            ||
+             ! extractSignature()   ||
+             ! decryptDigest()      ||
+             ! computeDigest()      ||
+             ! compareDigests()
+             )
+        {
+            clean();
+            return false;
+        }
+
+        return true;
+    }
+private:
+    bool init(const char* t, const char* k)
+    {
+        if (t==0 || k==0)
+            return false;
+
+        clean();
+        _tgz_name=t;
+        _tgz_basename=basename(t);
+        _key_name=k;
+
+        assert(_tgz_name!=0);
 
         _fd = open(_tgz_name, O_RDONLY);
         if (_fd==-1)
@@ -51,14 +79,26 @@ public:
         _sig_off = (off_t)-1;
 
         _sizeof_pkcs_mark = strlen("PKCSSIG()= ") + strlen(_tgz_basename);
-        _sizeof_dgst = strlen("SHA256()= ") + strlen(_tgz_basename) + SHA256_SIZE*2;
+        _sizeof_dgst_mark = strlen("SHA256()= ") + strlen(_tgz_basename);
+        _sizeof_dgst = _sizeof_dgst_mark + SHA256_SIZE*2;
         _sizeof_sig = _sizeof_pkcs_mark + _sizeof_pkcs_modulo;
-        free(_sig); _sig = nullptr;
-        free(_dgstD); _dgstD = nullptr;
-        free(_dgstC); _dgstC = nullptr;
+        free(_sig); _sig = 0;
+        free(_dgstD); _dgstD = 0;
+        free(_dgstC); _dgstC = 0;
         return true;
     }
-    void Clean()
+    /*@
+     * ensures this->_fd==-1;
+       ensures this->_sig_off==-1;
+       ensures this->_sizeof_sig==-1;
+       ensures this->_sizeof_dgst==-1;
+       ensures this->_sizeof_pkcs_mark==-1;
+       ensures this->_sig_off == -1;
+       ensures this->_sig == 0;
+       ensures this->_dgstD == 0;
+       ensures this->_dgstC == 0;
+     */
+    void clean()
     {
         if (_fd>-1) close(_fd);
 
@@ -67,17 +107,35 @@ public:
         _sizeof_sig=(off_t)-1;
         _sizeof_dgst=(off_t)-1;
         _sizeof_pkcs_mark=(off_t)-1;
+        _sizeof_dgst_mark=(off_t)-1;
         _sig_off = (off_t)-1;
-        free(_sig); _sig = nullptr;
-        free(_dgstD); _dgstD = nullptr;
-        free(_dgstC); _dgstC = nullptr;
+        free(_sig); _sig = 0;
+        free(_dgstD); _dgstD = 0;
+        free(_dgstC); _dgstC = 0;
     }
-    bool ExtractSignature()
+    /*@
+     * requires this->_sig==0;
+     * requires this->_fd>-1;
+     * requires this->_sizeof_sig!=-1);
+     * requires this->_sig==0;
+     * requires this->_sizeof_pkcs_modulo!=-1;
+     * requires this->_sizeof_pkcs_mark!=-1;
+     * behaviour failure:
+     *   assigns \nothing;
+     *   ensures \result==false;
+     *   assigns this->_sig_off;
+     * behaviour success:
+     *   assigns this->_sig_off;
+     *   assigns this->_sig;
+     *   assigns this->_sig[0..this->_sizeof_pkcs_modulo];
+     *   ensures \result==true;
+     */
+    bool extractSignature()
     {
         assert(_sig_off==(off_t)-1);
-        assert(_fd!=-1);
+        assert(_fd>-1);
         assert(_sizeof_sig!=(off_t)-1);
-        assert(_sig==nullptr);
+        assert(_sig==0);
         assert(_sizeof_pkcs_modulo!=(off_t)-1);
         assert(_sizeof_pkcs_mark!=(off_t)-1);
 
@@ -89,12 +147,12 @@ public:
         }
 
         uint8_t *mrk_sig = (uint8_t*)calloc(_sizeof_sig, sizeof(uint8_t));
-
         int rv = read(_fd, mrk_sig, _sizeof_sig);
         if (rv<=0)
         {
-            perror("failed to open signature len");
+            perror("failed to read signature");
             free(mrk_sig);
+            _sig_off=(off_t)-1;
             return false;
         }
 
@@ -105,18 +163,18 @@ public:
         return true;
     }
 
-    bool DecryptDigest()
+    bool decryptDigest()
     {
         int len = 0;
-        uint8_t *public_key = nullptr;
+        uint8_t *public_key = 0;
         int rv = 0;
         X509_CTX x509_ctx;
         int offset = 0;
 
-        assert(_key_name!=nullptr);
-        assert(_dgstD==nullptr);
+        assert(_key_name!=0);
+        assert(_dgstD==0);
         assert(_sizeof_pkcs_modulo!=(off_t)-1);
-        assert(_sig!=nullptr);
+        assert(_sig!=0);
 
         memset(&x509_ctx,0,sizeof(x509_ctx));
 
@@ -131,6 +189,7 @@ public:
         if (rv!=X509_OK)
         {
             fprintf(stderr, "unable to extract public key\n");
+            free(public_key);
             return false;
         }
 
@@ -139,13 +198,15 @@ public:
         if (rv<=0)
         {
             fprintf(stderr, "Failed to decrypt digest\n");
+            free(public_key);
             return false;
         }
 
+        free(public_key);
         RSA_free(x509_ctx.rsa_ctx);
         return true;
     }
-    bool ComputeDigest()
+    bool computeDigest()
     {
         int rv=0;
         uint8_t buf[512];
@@ -154,7 +215,8 @@ public:
 
         assert(_fd!=-1);
         assert(_sig_off!=(off_t)-1);
-        assert(_dgstC==nullptr);
+        assert(_dgstC==0);
+        assert(_sizeof_dgst_mark!=(off_t)-1);
 
         memset(buf, 0, sizeof(buf));
         memset(&sha256_ctx, 0, sizeof(sha256_ctx));
@@ -190,25 +252,30 @@ public:
         }
         SHA256_Final(sha256_dgst, &sha256_ctx);
 
-        _dgstC = (uint8_t*)calloc(_sizeof_dgst,sizeof(uint8_t));
+        _dgstC = (uint8_t*)calloc(_sizeof_dgst + 1 // \0
+                                 ,sizeof(uint8_t));
 
-        int o = sprintf((char*)_dgstC, "SHA256(%s)= ",_tgz_basename);
+        int o = snprintf((char*)_dgstC, _sizeof_dgst_mark + 1,
+                         "SHA256(%s)= ",_tgz_basename);
         for (int i=0; i<SHA256_SIZE; ++i)
         {
             sprintf((char*)_dgstC+o+i*2, "%02x", sha256_dgst[i]);
         }
         return true;
     }
-    void printDgst(uint8_t *dgst)
+    void printDgst(uint8_t *dgst) const
     {
+        assert(_sizeof_dgst!=(off_t)-1);
         for (int i=0; i<_sizeof_dgst; ++i)
+        {
             printf("%c",dgst[i]);
+        }
         printf("\n");
     }
-    bool CompareDigests()
+    bool compareDigests() const
     {
-        assert(_dgstD!=nullptr);
-        assert(_dgstC!=nullptr);
+        assert(_dgstD!=0);
+        assert(_dgstC!=0);
         assert(_sizeof_dgst!=(off_t)-1);
         for (int i=0;i<_sizeof_dgst; ++i)
         {
@@ -225,8 +292,6 @@ public:
 };
 
 
-
-
 int main(int argc, char* argv[])
 {
     if (argc!=3)
@@ -238,21 +303,9 @@ int main(int argc, char* argv[])
     const char* tgzfile = argv[1];
     const char* keyfile = argv[2];
 
-    Goopa g = Goopa(tgzfile, keyfile);
+    Goopa g = Goopa();
 
-    if ( ! g.Init())
-        return -1;
-
-    if ( ! g.ExtractSignature())
-        return -1;
-
-    if ( ! g.DecryptDigest())
-        return -1;
-
-    if ( ! g.ComputeDigest())
-        return -1;
-
-    if ( ! g.CompareDigests())
+    if ( ! g.Verify(tgzfile, keyfile))
         return -1;
 
     return 0;
