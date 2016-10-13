a3c24afb72210540d376d498730e4369bbb5401b
Adrian Negreanu <groleo@gmail.com>
2016-10-13 20:15:32 +0300

    add LGPL v2.1 licensing

--- tgz-verify.cpp
+++ tgz-verify.cpp
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2016  Adrian Marius Negreanu
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this proram; if not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <assert.h>
 #include <fcntl.h>
 #include <stdint.h>
@@ -9,10 +26,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-
 #include <axTLS/crypto.h>
 #include <axTLS/crypto_misc.h>
-
 
 class Goopa
 {
@@ -290,7 +305,6 @@ private:
         return true;
     }
 };
-
 
 int main(int argc, char* argv[])
 {
